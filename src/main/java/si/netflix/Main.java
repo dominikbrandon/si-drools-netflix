22f8e0ca754862ea64b66e63ce6a4f8af593f70e
Dominik Grzelak <grzelak.dominik@gmail.com>
2018-12-01 10:39:47 +0100

    Initial commit

---
+++ src/main/java/si/netflix/Main.java
@@ -0,0 +1,21 @@
+package si.netflix;
+
+import org.kie.api.KieServices;
+import org.kie.api.logger.KieRuntimeLogger;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // load up the knowledge base
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieSession kSession = kContainer.newKieSession("ksession-rules");
+        KieRuntimeLogger kLogger = ks.getLoggers().newFileLogger(kSession, "test");
+
+        // go !
+        kSession.fireAllRules();
+        kLogger.close();
+    }
+}
