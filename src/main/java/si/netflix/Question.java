2908d94fd05392bcc33a1eadee0115e7dee8daa5
Dominik Grzelak <grzelak.dominik@gmail.com>
2018-12-01 18:19:28 +0100

    Fix bug in rules

--- src/main/java/si/netflix/Question.java
+++ src/main/java/si/netflix/Question.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum Question {
     DRAMA_COMEDY("Drama or Comedy", "Drama", "Comedy", "A little bit of both"),
     SCI_FI("Do you like sci-fi or supernatural shows", "Yes", "No"),
-    QUES_MORALS("Do you like your character have questionable morals", "No", "Yes"),
+    QUEST_MORALS("Do you like your character have questionable morals", "No", "Yes"),
     SMALL_PRISON("Small town or prison", "Small Town", "Prison"),
     STRONG_FEMALE("Do you like strong female charaters", "Yes", "No"),
     POLITICS("Politics", "Yes", "No"),
