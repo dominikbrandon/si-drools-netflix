450f5653488e168595751ee9322fa0bb8a4cf334
Mateusz Wisniewski <mw910712@wmservice.corpnet1.com>
2018-12-03 13:46:59 +0100

    All questions added

--- src/main/java/si/netflix/Question.java
+++ src/main/java/si/netflix/Question.java
@@ -12,7 +12,24 @@ public enum Question {
     POLITICS("Politics", "Yes", "No"),
     CAN_GORE("Can you handle gore?","Yes", "No"),
     FRIEND_DYSF("Friend group or dysfunctional family","Friends", "Family"),
-    ;
+    BRITISH_CHAR("British charakters?", "No", "Yes"),
+    FEEL_CRY("Feel like crying?", "Yes", "No"),
+    SELL_DRUGS("Do you like them to sell drugs?","Yes","No"),
+    NEED_ACTION("Need action?", "Yes", "No"),
+    WORK_HUMOR("Work place humor?", "No", "Yes"),
+    FOOTBALL("Do you like football?", "Yes", "No"),
+    ROMANCE("Scandalous romance?", "Yes", "No"),
+    CRIME_SHOWS("Do you like crime shows?", "Yes", "No"),
+    HIGH_SCHOOL("Want to see James Franco & Seth Rogen as high schoolers?", "No", "Yes"),
+    ZOMBIES("Do you like zombies?", "Yes", "No"),
+    HOSPITALS("Hospitals?", "No", "Yes"),
+    TINA_AMY("Tina Fey or Amy Poehler?", "Tina", "Amy"),
+    ROOT_CRIMINAL("Do you root for the criminals?", "Yes", "No"),
+    METH("Meth?", "Yes", "No"),
+    OPTI_CYNIC("Optimistic or cynical?", "Optimistic", "Cynical"),
+    VAMP("Vampires?", "Yes", "No"),
+    BAR_CAFE("Do you want your NYC friends to hang out at a Bar or Cafe?", "Bar", "Cafe"),
+    FOREN_PSYC("Forensics or psychology?", "Forensics", "Psychology");
 
     private final String text;
     private final String[] options;
